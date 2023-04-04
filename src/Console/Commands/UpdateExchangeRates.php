493be46cc10b8ba95dea98d11aaa724198a4d700
lowkey1729 <olarewajumojeed9@gmail.com>
2023-04-04 01:17:46 +0100

    Built a command that updates the exhange rates

---
+++ src/Console/Commands/UpdateExchangeRates.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Mojeed\BuckhillCurrencyConverter\Console\Commands;
+
+use Exception;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Mojeed\BuckhillCurrencyConverter\Services\APIs\EuropeanCentralBankRates;
+
+final class UpdateExchangeRates extends Command
+{
+    protected $signature = 'buckhill:update-exchange-rates';
+
+    protected $description = 'Update the currency rate for each primary currency.';
+
+    /**
+     * @return int
+     * @throws Exception
+     */
+    public function handle(): int
+    {
+        if ($this->tableExistsInTheDB('exchange_rates')) {
+            DB::table('exchange_rates')->truncate();
+            $exchangeRates = $this->getExchangeRates();
+            $currencyRate = [];
+            $this->loadCurrencyRate($exchangeRates['data'], $currencyRate);
+            DB::table('exchange_rates')->insert($currencyRate);
+
+            $this->info('Exchange rates updated successfully!');
+        }
+
+
+        return 0;
+    }
+
+    /**
+     * @param array $exchangeRates
+     * @param array $currencyRate
+     * @return void
+     */
+    protected function loadCurrencyRate(
+        array $exchangeRates,
+        array &$currencyRate
+    ): void {
+        foreach ($exchangeRates as $key => $exchangeRate) {
+            $currencyRate[] = [
+                'currency_to_exchange' => $exchangeRate['@attributes']['currency'],
+                'rate' => $exchangeRate['@attributes']['rate'],
+                'primary_currency' => 'EUR'
+
+            ];
+        }
+    }
+
+    /**
+     * @return array
+     * @throws Exception
+     */
+    protected function getExchangeRates(): array
+    {
+        /* @var  EuropeanCentralBankRates $service */
+        $service = resolve(EuropeanCentralBankRates::class);
+        return $service->fetchRates();
+    }
+
+    /**
+     * @param string $tableName
+     * @return bool
+     */
+    protected function tableExistsInTheDB(string $tableName): bool
+    {
+        return Schema::hasTable($tableName);
+    }
+}
